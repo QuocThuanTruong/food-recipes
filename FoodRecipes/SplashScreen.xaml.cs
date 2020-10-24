01da8ccc8dcd88f770fc0cfc9a08173d11dba060
THUẬN TRƯƠNG QUỐC <18120583@student.hcmus.edu.vn>
2020-10-24 10:41:03 +0700

    complete layout splash screen

--- FoodRecipes/SplashScreen.xaml.cs
+++ FoodRecipes/SplashScreen.xaml.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Security.RightsManagement;
 using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -14,6 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Configuration;
+using System.Timers;
+using System.Diagnostics;
 
 namespace FoodRecipes
 {
@@ -22,6 +24,16 @@ namespace FoodRecipes
 	/// </summary>
 	public partial class SplashScreen : Window
 	{
+		#region Private Fields
+		private Timer loadingTmer;
+		private Configuration configuration;
+		private int timeCounter = 0;
+		private string isShowSplashValue = "True";
+
+		private const int TIME_LOAD_UNIT = 1000;
+		private const int TOTAL_TIME_LOAD_IN_SECOND = 5;
+		#endregion
+
 		public SplashScreen()
 		{
 			InitializeComponent();
@@ -30,7 +42,65 @@ namespace FoodRecipes
 
 		private void Window_Loaded(object sender, RoutedEventArgs e)
 		{
+			//Get splash screen config value
+			configuration = ConfigurationManager.OpenExeConfiguration(ConfigurationUserLevel.None);
+			var appSettingValue = ConfigurationManager.AppSettings["ShowSplashScreen"];
+			var isSplashScreenShow = bool.Parse(appSettingValue);
+			Debug.WriteLine(isSplashScreenShow);
 
+			if (isSplashScreenShow)
+			{
+				loadingTmer = new Timer(TIME_LOAD_UNIT);
+				loadingTmer.Elapsed += LoadingTmer_Elapsed;
+				loadingTmer.Start();
+			}
+			else
+			{
+				showHomeScreen();
+			}
+		}
+
+		/// <summary>
+		/// Show home screen and close splash screen
+		/// </summary>
+		private void showHomeScreen()
+		{
+			var homeScreen = new HomeScreen();
+
+			this.Close();
+			homeScreen.Show();	
+		}
+
+		private void LoadingTmer_Elapsed(object sender, ElapsedEventArgs e)
+		{
+			Dispatcher.Invoke(() =>
+			{
+				timeCounter++;
+
+				if (timeCounter == TOTAL_TIME_LOAD_IN_SECOND)
+				{
+					loadingTmer.Stop();
+					showHomeScreen();
+				}
+
+				Debug.WriteLine(timeCounter);
+			});
+		}
+
+		private void turnOffSplashCheckBox_Checked(object sender, RoutedEventArgs e)
+		{
+			isShowSplashValue = "False";
+		}
+
+		private void turnOffSplashCheckBox_Unchecked(object sender, RoutedEventArgs e)
+		{
+			isShowSplashValue = "True";
+		}
+
+		private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+		{
+			configuration.AppSettings.Settings["ShowSplashScreen"].Value = isShowSplashValue;
+			configuration.Save(ConfigurationSaveMode.Minimal);
 		}
 	}
 }
--- FoodRecipes/StringValue.Designer.cs
+++ FoodRecipes/StringValue.Designer.cs
@@ -79,6 +79,15 @@ namespace FoodRecipes {
         }
         
         /// <summary>
+        ///   Looks up a localized string similar to Không hiển thị Splash Screen khi khởi chạy ứng dụng vào lần tới..
+        /// </summary>
+        public static string message_not_show_splash {
+            get {
+                return ResourceManager.GetString("message_not_show_splash", resourceCulture);
+            }
+        }
+        
+        /// <summary>
         ///   Looks up a localized string similar to Gà lăn hồ đào mỹ chiên giòn với salad nho khô dưa hấu là món salad vừa thơm ngon lại rất bổ dưỡng. Một món ăn chứa đầy đủ chất dinh dưỡng từ các loại rau xanh, dưa hấu, nho khô, thịt gà ăn kèm cùng với sốt giấm đặc biệt sẽ khiến cho bạn càng ăn càng mê. Bạn hãy lưu lại ngay công thức bên dưới để làm món salad này nhé..
         /// </summary>
         public static string test_description {
